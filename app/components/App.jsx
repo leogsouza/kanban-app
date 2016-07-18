1f6bd96f24d3abe91d1d1d72525fde2f648d1d0a
Leonardo Souza <leogsouza@gmail.com>
2016-07-17 22:40:01 -0300

    Pushing state to App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,4 +1,46 @@
 import React from 'react';
+
+import uuid from 'uuid';
 import Notes from './Notes';
 
-export default () => <Notes />;
+export default class App extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: [
+              {
+                id: uuid.v4(),
+                task: 'Learn React'
+              },
+              {
+                id: uuid.v4(),
+                task: 'Do laundry'
+              },
+              {
+                id: uuid.v4(),
+                task: 'Learn Webpack'
+              },
+              {
+                id: uuid.v4(),
+                task: 'Learn AngularJS'
+              },
+              {
+                id: uuid.v4(),
+                task: 'Learn Golang'
+              }
+            ]
+        };
+    }
+
+    render() {
+        const {notes} = this.state;
+
+        return (
+            <div>
+                <button onClick={ () => console.log('add note')}>+</button>
+                <Notes notes={notes} />
+            </div>
+        );
+    }
+}
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,27 +1,7 @@
 import React from 'react';
-import uuid from 'uuid';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn AngularJS'
-  }
-];
-
-export default () => (
-	<ul>{notes.map(note => 
+export default ({notes}) => (
+	<ul>{notes.map(note =>
 		<li key={note.id}>{note.task}</li>
 	)}</ul>
-)
+);
