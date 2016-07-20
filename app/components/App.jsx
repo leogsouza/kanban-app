0b0b39762d3f7b21347e41329430be140af2021e
Leonardo Souza <leogsouza@gmail.com>
2016-07-20 17:39:56 -0300

    Implementing Editing for Lane names

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,48 +4,6 @@ import connect from '../libs/connect';
 import Lanes from './Lanes';
 import LaneActions from '../actions/LaneActions';
 
-/*class App extends React.Component{
-    
-    render() {
-        const {notes} = this.props;
-
-        return (
-            <div>                
-                <button className="add-note" onClick={ this.addNote}>+</button>
-                <Notes 
-                    notes={notes} 
-                    onNoteClick={this.activateNoteEdit}
-                    onEdit={this.editNote} 
-                    onDelete={this.deleteNote} 
-                />
-            </div>
-        );
-    }
-
-    addNote = () => {
-        this.props.NoteActions.create({
-            id: uuid.v4(),
-            task: 'New Task'
-        });        
-    }
-
-    deleteNote = (id, e) => {
-        // Avoiding bubble to edit
-        e.stopPropagation();
-
-        this.props.NoteActions.delete(id);
-    }
-
-    activateNoteEdit = (id) => {
-        this.props.NoteActions.update({id, editing: true});
-    }
-
-    editNote = (id, task) => {
-        const {NoteActions} = this.props;
-
-        NoteActions.update({id, task, editing: false});
-    }
-}*/
 const App = ({LaneActions, lanes}) => {
     const addLane = () => {
         LaneActions.create({
--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -17,6 +17,18 @@ export default class LaneStore {
 		});
 	}
 
+	update(updatedLane) {
+		this.setState({
+			lanes: this.lanes.map( lane => {
+				if(lane.id === updatedLane.id) {
+					return Object.assign({}, lane, updatedLane);
+				}
+
+				return lane;
+			})
+		});
+	}
+
 	attachToLane({laneId, noteId}) {
 		this.setState({
 			lanes: this.lanes.map(lane => {
