a57d3b42b791c4178e6745895b58cfb192fdb71d
Leonardo Souza <leogsouza@gmail.com>
2016-07-20 19:04:45 -0300

    Implementing Lane Deletion

--- app/components/LaneHeader.jsx
+++ app/components/LaneHeader.jsx
@@ -39,13 +39,23 @@ export default connect(() => ({}), {
 		});
 	};
 
+	const deleteLane = e => {
+		// Avoid bubbling to edit
+		e.stopPropagation();
+
+		LaneActions.delete(lane.id);
+	}
+
 	return (
 		<div className="lane-header" onClick={activateLaneEdit} {...props}>
 			<div className="lane-add-note">
 				<button onClick={addNote}>+</button>
 			</div>
 			<Editable className="lane-name" editing={lane.editing}
-				value={lane.name} onEdit={editName} />			
+				value={lane.name} onEdit={editName} />
+			<div className="lane-delete">
+				<button onClick={deleteLane}>x</button>
+			</div>		
 		</div>
 	);
-})
+});
