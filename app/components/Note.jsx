46ba4e55a018800809231d2d1b5ae4d827717c0b
Leonardo Souza <leogsouza@gmail.com>
2016-07-21 15:21:33 -0300

    Allowing Notes to Be Dragged, Allowing Notes to Detect Hovered Notes

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import {compose} from 'redux';
+import {DragSource, DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 
-export default ({children, ...props}) => (
-	<div {...props}>
-		{children}		
-	</div>
-);
+const Note = ({
+	connectDragSource, connectDropTarget, children, ...props
+}) => {
+	return compose(connectDragSource,connectDropTarget)(
+		<div {...props}>
+			{children}
+		</div>
+	);
+};
+
+const noteSource = {
+	beginDrag(props) {
+		console.log('begin dragging note', props);
+
+		return {};
+	}
+};
+
+const noteTarget = {
+	hover(targetProps, monitor) {
+		const sourceProps = monitor.getItem();
+
+		console.log('dragging note', sourceProps, targetProps);
+	}
+};
+
+export default compose(
+	DragSource(ItemTypes.NOTE, noteSource, connect => ({
+		connectDragSource: connect.dragSource()
+	})),
+	DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
+		connectDropTarget: connect.dropTarget()
+	}))
+)(Note)
